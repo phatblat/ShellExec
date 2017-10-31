5cd3ee1d26bdec316df27b92edc9ee92e05e72cf
Ben Chatelain <ben@octop.ad>
2017-10-31 17:03:55 -0600

    WIP: Add test for failing command

--- src/test/kotlin/at.phatbl.simple_exec/SimpleExecSpek.kt
+++ src/test/kotlin/at.phatbl.simple_exec/SimpleExecSpek.kt
@@ -4,6 +4,7 @@ import org.gradle.testfixtures.ProjectBuilder
 import org.jetbrains.spek.api.Spek
 import org.jetbrains.spek.api.dsl.describe
 import org.jetbrains.spek.api.dsl.it
+import kotlin.test.assertEquals
 import kotlin.test.assertNotNull
 
 class SimpleExecSpec: Spek({
@@ -17,11 +18,31 @@ class SimpleExecSpec: Spek({
 
         it("can run a simple command") {
             task.command = "true"
+
             task.execute()
+            while (task.state.executing || !task.state.executed) {
+                Object().wait(10)
+            }
 
             val result = task.execResult
             assertNotNull(result)
             result.assertNormalExitValue()
+        }
+
+        it("can run a failing command") {
+            task.command = "false"
+            task.setIgnoreExitValue(true)
+
+            task.execute()
+            synchronized(task.state) {
+                while (task.state.executing || !task.state.executed) {
+                    Object().wait(10)
+                }
+            }
+
+            val result = task.execResult
+            assertNotNull(result)
+            assertEquals(1, result.exitValue)
         }
     }
 })
