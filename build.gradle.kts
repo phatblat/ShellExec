42f5731faecefac15f489d47e6f1ee47cefcd654
Ben Chatelain <ben@octop.ad>
2017-11-10 12:44:33 -0700

    Update gradle wrapper to 4.3.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -56,7 +56,7 @@ val removeBatchFile by tasks.creating(Delete::class) { delete("gradlew.bat") }
 
 tasks {
     "wrapper"(Wrapper::class) {
-        gradleVersion = "4.3"
+        gradleVersion = "4.3.1"
         distributionType = DistributionType.ALL
         finalizedBy(removeBatchFile)
     }
