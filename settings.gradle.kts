1a0af7d893565ff863cd39450e5316d2174e514f
Ben Chatelain <ben@octop.ad>
2019-07-05 17:04:47 -0600

    🚨 Remove deprecated STABLE_PUBLISHING feature enable

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -5,8 +5,6 @@
 
 rootProject.name = "ShellExec"
 
-enableFeaturePreview("STABLE_PUBLISHING")
-
 // Workaround to make the JUnit Platform Gradle Plugin available using the `plugins` DSL
 // See: https://github.com/junit-team/junit5/issues/768#issuecomment-330078905
 pluginManagement {
