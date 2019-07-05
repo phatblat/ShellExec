23c69f146888a720b549c31dac774fffd86acb1c
Ben Chatelain <ben@octop.ad>
2019-07-05 16:55:26 -0600

    ➖🔌 Clamp

--- build.gradle.kts
+++ build.gradle.kts
@@ -37,7 +37,6 @@ plugins {
 
     // Gradle plugin portal - https://plugins.gradle.org/
     kotlin("jvm") version "1.3.41"
-    id("at.phatbl.clamp") version "1.1.0"
     id("at.phatbl.shellexec") version "1.1.3"
     id("com.gradle.plugin-publish") version "0.9.10"
     id("com.jfrog.bintray") version "1.8.0"
