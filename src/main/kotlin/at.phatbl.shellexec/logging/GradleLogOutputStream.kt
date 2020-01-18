c72d0ec850dfd42c75e80f08c4ff3eae5d215db4
Ben Chatelain <ben@octop.ad>
2020-01-17 20:59:23 -0700

    🐛 Change annotation on output properties
    @Input properties must be serializable.

--- src/main/kotlin/at.phatbl.shellexec/logging/GradleLogOutputStream.kt
+++ src/main/kotlin/at.phatbl.shellexec/logging/GradleLogOutputStream.kt
@@ -9,7 +9,11 @@ import java.io.File
 /**
  * Adapter which passes log output through to Gradle's logger.
  */
-class GradleLogOutputStream(val logger: Logger, level: LogLevel, val logFiles: Array<File>? = null): LogOutputStream(level.ordinal) {
+class GradleLogOutputStream(
+    val logger: Logger,
+    level: LogLevel,
+    val logFiles: Array<File>? = null
+): LogOutputStream(level.ordinal) {
     /**
      * Logs a line to the log system of the user.
      *
